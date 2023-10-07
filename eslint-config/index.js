d0f7f443c20e7a4b25df98fa7ddc4cb7b8d729fd
Siarhei Pismennyi <siarhei_pismennyi@epam.com>
2023-10-07 23:21:33 +0200

    turborepo init

---
+++ eslint-config/index.js
@@ -0,0 +1,52 @@
+/** @type {import("@types/eslint").Linter.Config} */
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  settings: {
+    typescript: true,
+    node: true,
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json',
+      },
+    },
+  },
+  rules: {
+    // turn on errors for missing imports
+    'import/no-unresolved': 'error',
+    // 'import/no-named-as-default-member': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin', // Built-in types are first
+          ['sibling', 'parent'], // Then sibling and parent types. They can be mingled together
+          'index', // Then the index file
+          'object',
+        ],
+        alphabetize: {
+          /* sort in ascending order. Options: ["ignore", "asc", "desc"] */
+          order: 'asc',
+          /* ignore case. Options: [true, false] */
+          caseInsensitive: true,
+        },
+      },
+    ],
+    '@typescript-eslint/array-type': ['error', { default: 'generic' }],
+  },
+};
---
+++ prettier-config/index.js
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+};
