030fe10fff090e0d7d78b22309538a0753167924
Siarhei Pismennyi <siarhei_pismennyi@epam.com>
2023-10-08 15:42:14 +0200

    CORS fix

--- product-service/src/utils/response.ts
+++ product-service/src/utils/response.ts
@@ -2,5 +2,9 @@ import { APIGatewayProxyResult } from 'aws-lambda';
 
 export const parseResponse = (statusCode: number, response: unknown): APIGatewayProxyResult => ({
   statusCode,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': 'true',
+  },
   body: typeof response === 'string' ? response : JSON.stringify(response),
 });
